58d0d679462c7310a2fe4697214cc69dc0234de1
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-06-03 12:06:23 +0300

    Update Form.php

--- src/hachkingtohach1/SkyWars/form/Form.php
+++ src/hachkingtohach1/SkyWars/form/Form.php
@@ -26,6 +26,8 @@ namespace hachkingtohach1\SkyWars\form;
 
 use pocketmine\player\Player;
 use hachkingtohach1\SkyWars\SkyWars;
+use Vecnavium\FormsUI\Form;
+use Vecnavium\FormsUI\SimpleForm;
 
 class Form{
 
