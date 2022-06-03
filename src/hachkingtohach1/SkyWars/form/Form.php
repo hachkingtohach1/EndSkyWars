7e48ea7d69569394588a3c44cc38dfce819bbd2b
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-06-03 12:08:23 +0300

    Update Form.php

--- src/hachkingtohach1/SkyWars/form/Form.php
+++ src/hachkingtohach1/SkyWars/form/Form.php
@@ -29,7 +29,7 @@ use hachkingtohach1\SkyWars\SkyWars;
 use Vecnavium\FormsUI\Form;
 use Vecnavium\FormsUI\SimpleForm;
 
-class Form implements Form{
+class Form implements IForm{
 
   /**
     * @param Player $player
