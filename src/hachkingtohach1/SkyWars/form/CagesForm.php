c9cbfdbe8e5076eb27b20bdf138d105fc7f8900c
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-08-24 21:31:45 +0200

    Update CagesForm.php

--- src/hachkingtohach1/SkyWars/form/CagesForm.php
+++ src/hachkingtohach1/SkyWars/form/CagesForm.php
@@ -44,7 +44,7 @@ class CagesForm{
 			    return true;
 			}
 		    if(isset(Cosmetics::CAGES[$result])){
-				if(!$player->hasPermission(Cosmetics::PERMISSION_CAGES.".".str_replace(" ", "", strtolower(Cosmetics::CAGES[$result])))){
+				if(!$player->hasPermission("skywars.cage")){
 				    $player->sendMessage(TextFormat::RED."You don't have permission!");
 					return false;
 				}
@@ -59,7 +59,7 @@ class CagesForm{
 		    if(SkyWars::getInstance()->getCosmetics()->checkCagePlayer($player, $case)){
 			    $form->addButton(TextFormat::GREEN.$cage, 0, "");
 			}else{
-				if($player->hasPermission(Cosmetics::PERMISSION_CAGES.".".str_replace(" ", "", strtolower($cage)))){
+				if($player->hasPermission("skywars.cage")){
 				    $form->addButton(TextFormat::GOLD.$cage, 0, "");
 				}else{
 					$form->addButton(TextFormat::RED.$cage, 0, "");
